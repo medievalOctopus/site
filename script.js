1fbdd7d12d0932a4698fc9dbdd9e5023ed23c933
medievalOctopus <email4spam@null.net>
2015-04-11 19:48:19 +0100

    did website

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-$(document).ready(function(){
-  
-});
+console.log("Enter password in the format: pswd('@') where @ is the password.");
+var pswd = function(foo){
+	if (foo==="foobar"){
+		alert("Password accepted! You will be taken to the other site.");
+		window.location.assign("http://www.w3schools.com")
+	}
+};
